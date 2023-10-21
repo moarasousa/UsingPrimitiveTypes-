35081bedcdf20508133bbc214972d9944a7c763c
Moara Sousa <moara.sousa15@outlook.com>
2023-10-21 00:10:32 -0300

    UsingPrimitivesTypes

---
+++ TiposPrimitivos/src/tiposprimitivossaída/TiposPrimitivosSaida.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivossaída;
+
+
+/**
+ *
+ * @author HOME
+ */
+public class TiposPrimitivosSaida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Gustavo";
+        float nota = 8.5f;
+        System.out.printf("A nota de %s é %.2f \n", nome, nota);
+        
+        System.out.println("A nota do " + nome + " é: " + nota);
+                
+    }
+    
+}
